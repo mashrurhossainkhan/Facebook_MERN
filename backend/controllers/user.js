cdbdde51a1b497effcd81d3fc2df815f18251a44
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-06-07 23:09:31 +0600

    user authentication: registration and login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,11 +1,14 @@
 const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { generateToken } = require('../helpers/tokens');
+const { sendVerificationEmail } = require('../helpers/mailer');
 const {
   validateEmail,
   validateLength,
   validateUsername,
 } = require('../helpers/validation');
+
 exports.register = async (req, res) => {
   try {
     const {
@@ -68,12 +71,76 @@ exports.register = async (req, res) => {
       { id: user._id.toString() },
       '30m'
     );
-    console.log(emailVerificationToken);
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, '7d');
     res.send({
       id: user._id,
       username: user.username,
+      picture: user.picture,
       first_name: user.first_name,
       last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: 'Register Success ! please activate your email to start',
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+    if (check.verified == true) {
+      return res
+        .status(400)
+        .json({ message: 'this email is already activated' });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res
+        .status(200)
+        .json({ message: 'Account has beeen activated successfully.' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message:
+          'the email address you entered is not connected to an account.',
+      });
+    }
+    if (!user.verified) {
+      console.log('User Not Verified');
+      return res.status(400).json({
+        message: 'Your account is not verified yet, please verify first',
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: 'Invalid credentials.Please try again.',
+      });
+    }
+    const token = generateToken({ id: user._id.toString() }, '7d');
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: 'Login Successful',
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const { home, register } = require('../controllers/user');
+const {
+  home,
+  register,
+  activateAccount,
+  login,
+} = require('../controllers/user');
 const router = express.Router();
 
 router.post('/register', register);
+router.post('/activate', activateAccount);
+router.post('/login', login);
 
 module.exports = router;
