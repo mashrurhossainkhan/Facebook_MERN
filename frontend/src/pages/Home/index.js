b21230c17680daa22e807f01a1f8d37129a443db
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-06-08 15:56:18 +0600

    left sidebar

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,13 @@
-import { useRef, useState } from 'react';
 import Header from '../../components/header';
+import { useSelector } from 'react-redux';
+import LeftHome from '../../components/home/left';
 
 export default function Home() {
+  const { user } = useSelector((user) => ({ ...user }));
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
     </div>
   );
 }
