d75061ef4759ef7e5933c87f583b4238dc137ee2
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-06-08 14:16:49 +0600

    header

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import Header from '../../components/header';
 
 export default function Home() {
   return (
     <div>
-      <p>This is home page</p>
+      <Header />
     </div>
   );
 }
