4812ead188f15372dbf855aa136e4c65520ef3bb
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-06-08 12:48:28 +0600

    login registration

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import './style.css';
+import LoginForm from '../../components/login/LoginForm';
+import Footer from '../../components/login/Footer';
+import RegisterForm from '../../components/login/RegisterForm';
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const [visible, setVisible] = useState(false);
+  const [cookies, setCookies] = useState(Cookies.get('user'));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cookies != null) {
+      navigate('/');
+    }
+  });
   return (
-    <div>
-      <p>Login page</p>
+    <div className="login">
+      <div className="login_wrapper">
+        <LoginForm setVisible={setVisible} />
+        {visible && <RegisterForm setVisible={setVisible} />}
+        {/*The expression before the && operator (visible) is evaluated first. If visible is true, then the right-hand side of the && operator (<RegisterForm setVisible={setVisible} />) is returned and rendered.*/}
+        <Footer />
+      </div>
     </div>
   );
 }
