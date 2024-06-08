f8316d4a350b6331388542ab8811514fdfb1d475
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-06-08 17:39:00 +0600

    frontend

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,31 @@
+import './style.css';
 import Header from '../../components/header';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import LeftHome from '../../components/home/left';
+import RightHome from '../../components/home/right';
+import Stories from '../../components/home/stories';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import CreatePost from '../../components/createPost';
 
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
+  const [cookies, setCookies] = useState(Cookies.get('user'));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (cookies != null && user != null) {
+      dispatch({ type: 'LOGIN', payload: JSON.parse(cookies) });
+    }
+  });
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+        <CreatePost user={user} />
+      </div>
+      <RightHome user={user} />
     </div>
   );
 }
